b1ea2cf86a30d12b32c1d5452cb46ac605999d48
RyanMahajan <ryanmahajan2005@gmail.com>
2025-03-18 23:02:25 -0400

    template website added

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,28 +1,44 @@
-import * as React from 'react';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppTheme from '@/shared-theme/AppTheme';
-import AppAppBar from '@/components/AppAppBar';
-import theme from '@/theme';
-import InitColorSchemeScript from '@mui/material/InitColorSchemeScript';
-import ModeSwitch from '@/components/ModeSwitch';
+'use client';
 
-export default function RootLayout(props: { children: React.ReactNode }) {
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import AppTheme from '../shared-theme/AppTheme';
+import AppAppBar from '../components/AppAppBar';
+import Hero from '../components/Hero';
+import LogoCollection from '../components/LogoCollection';
+import Highlights from '../components/Highlights';
+import Pricing from '../components/Pricing';
+import Features from '../components/Features';
+import Testimonials from '../components/Testimonials';
+import FAQ from '../components/FAQ';
+import Footer from '../components/Footer';
+
+export default function MarketingPage(props: { disableCustomTheme?: boolean }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <InitColorSchemeScript attribute="class" />
-        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <AppTheme {...props}>
-            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-            <CssBaseline enableColorScheme />
-	    <AppAppBar />
-            <ModeSwitch />
-            {props.children}
-          </AppTheme>
-        </AppRouterCacheProvider>
-      </body>
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+
+      <AppAppBar />
+      <Hero />
+      <div>
+        <LogoCollection />
+        <Features />
+        <Divider />
+        <Testimonials />
+        <Divider />
+        <Highlights />
+        <Divider />
+        <Pricing />
+        <Divider />
+        <FAQ />
+        <Divider />
+        <Footer />
+      </div>
+    </AppTheme>
+    </body>
     </html>
   );
 }
